c7c51917976a37f93d6835e347b84ef951ec2c7c
Daniel Lisboa <86170130+danlisb@users.noreply.github.com>
2023-08-23 17:15:55 -0300

    Update db.py

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ con = psycopg2.connect(
     host = "localhost",
     database = "spotify_pbd",
     user = "postgres",
-    password = "6661"
+    password = ""
 )  
 
 # inicia o cursor para executar as queries
